6160350251bb8b6bdb0557698e63952a98ba2a56
yan4iska <matveev.git@gmail.com>
2024-09-18 12:12:16 +0300

    Auth service
    сделал базовый auth сервис и контроллер

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-
+import { UserModule } from "../user/user.module";
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from "@nestjs/jwt";
+import { getJwtConfig } from "../config/jwt.config";
+import { JwtStrategy } from "./jwt.strategy";
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  imports: [UserModule, ConfigModule, JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: getJwtConfig
+  })],
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
