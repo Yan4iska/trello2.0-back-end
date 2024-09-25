a47487b7901c1eb17df55d8503a8ab56f89fbce1
yan4iska <matveev.git@gmail.com>
2024-09-26 02:12:07 +0300

    user logic is end
    end of user
    add tasks logic for future {TODO}

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,38 @@
+import { IsNumber, IsOptional, Min, Max, IsEmail, MinLength, IsString } from "class-validator";
+
+
+
+export class PomodoroSettingsDto {
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  workInterval?: number
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  breakInterval?: number
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Max(10)
+  intervalsCount?: number
+}
+
+export class UserDto extends PomodoroSettingsDto {
+  @IsEmail()
+  @IsOptional()
+  email: string
+
+  @IsOptional()
+  @MinLength(6, {
+    message: "Password must be at least 6 characters long"
+  })
+
+  @IsOptional() 
+  @IsString()
+  password: string
+}
+
+
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from "../prisma.service";
 import { AuthDto } from "../auth/dto/auth.dto";
 import { hash } from "argon2";
+import { UserDto } from "./dto/user.dto";
+import { startOfDay, subDays } from "date-fns";
 
 @Injectable()
 export class UserService {
@@ -24,6 +26,56 @@ export class UserService {
     })
   }
 
+  async getProfile(id: string) {
+    const profile = await this.getById(id);
+
+    const totalTasks = profile.tasks.length
+    const completedTasks = await this.prisma.task.count({
+      where: {
+        userId: id,
+        isCompleted: true,
+      }
+    })
+
+
+
+    const todayStart = startOfDay(new Date())
+    const weekStart = startOfDay(subDays(new Date(), 7))
+
+    const todayTasks = this.prisma.task.count({
+      where: {
+        userId: id,
+        createdAt: {
+          gte: todayStart.toISOString()
+        }
+      }
+    })
+
+    const weekTasks = this.prisma.task.count({
+      where: {
+        userId: id,
+        createdAt: {
+          gte: weekStart.toISOString()
+        }
+      }
+    })
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const {password, ...rest} = profile
+    return {
+      user: rest,
+      statistics: [
+        {label: 'Total', val: totalTasks },
+        {label: 'Completed tasks', val: completedTasks},
+        {label: 'Today tasks', val: todayTasks},
+        {label: 'Week tasks', val: weekTasks},
+      ]
+    }
+
+  }
+
+
+
   async create(dto:AuthDto) {
     const user = {
       email: dto.email,
@@ -33,6 +85,24 @@ export class UserService {
 
     return this.prisma.user.create({
       data:user
+    })
+  }
+
+  async update(id:string, dto:UserDto) {
+    let data = dto
+    if (dto.password) {
+      data = {...dto, password: await hash(dto.password)}
+    }
+
+    return this.prisma.user.update({
+      where: {
+        id
+      },
+      data,
+      select: {
+        name: true,
+        email: true,
+      }
     })
   }
 }
